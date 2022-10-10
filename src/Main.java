a732a4c24a924c222c2e7d1172e3f0798caccb4a
AlinaSachuk <alinko-91@yandex.ru>
2022-10-10 21:45:43 +0300

    created new object from class Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,23 @@
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello world!!!!!!!");
-        System.out.println("Hello world!!!!!!!");
-        System.out.println("Hello world!!!!!!!");
-        System.out.println("Hello world!!!!!!!");
+    public static void main(String[] args) {
+        System.out.println("Запуская метод intArray");
+        int[] array = new int[10];
+        array = arrayPull(array);
+        System.out.println(Arrays.toString(array));
+        Calculator calc = new Calculator(100, 25, "red");
+        System.out.println("Сумма двух чисел = " + calc.sumCalculator(6, 8));
+        System.out.println("Разность двух чисел = " + calc.difference(6, 8));
+        System.out.println("Деление двух чисел = " + calc.division(6, 8));
+        System.out.println("Умножение двух чисел = " + calc.multiplication(6, 8));
+        System.out.println("калькулятор весом: " + calc.weight + ";" + " " + "стоимостью:" + calc.cost + ";" + " " + "цвет:" + calc.color);
     }
 
+    public static int[] arrayPull(int[] someArray) {
+        for (int i = 0; i < someArray.length; i++) {
+            someArray[i] = (int) (Math.random() * 100);
+        }
+        return someArray;
+    }
 }
